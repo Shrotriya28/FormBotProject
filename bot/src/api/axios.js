fa3339565eaa350035ac024cc7558c1f9f76b172
Harsh Shrotriya <160751771+Shrotriya28@users.noreply.github.com>
2024-08-15 17:57:11 +0530

    Update axios.js

--- bot/src/api/axios.js
+++ bot/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://formbotproject-1.onrender.com',  
+    baseURL: "https://formbotproject.onrender.com",  
 });
 
 instance.interceptors.request.use(
